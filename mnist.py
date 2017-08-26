8d1eba050624990be71f46a778127e359ec003c3
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-08-26 16:23:41 +0200

    Refactoring

--- mnist.py
+++ mnist.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-import keras
 # Feed forward
 from keras.models import Sequential
 # Core layers
@@ -19,18 +18,19 @@ from matplotlib import pyplot as plt
 # Ignore tensorflow CPU warnings as we are using the GPU build anyways
 # See https://github.com/tensorflow/tensorflow/issues/7778
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # Workaround for crash
 # See https://github.com/tensorflow/tensorflow/issues/6698
 import tensorflow as tf
+
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 K.set_session(sess)
 
-
 if __name__ == '__main__':
-    np.random.seed(123) 
+    np.random.seed(123)
 
     # Load pre-shuffled MNIST data into train and test sets
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -50,7 +50,6 @@ if __name__ == '__main__':
     print('Shape after setting depth to one:')
     print(X_train.shape)
 
-
     # Convert underlying data types
     X_train = X_train.astype('float32')
     X_test = X_test.astype('float32')
@@ -65,12 +64,11 @@ if __name__ == '__main__':
     # Convert 1-dimensional class arrays to 10-dimensional class matrices
     Y_train = np_utils.to_categorical(y_train, 10)
     Y_test = np_utils.to_categorical(y_test, 10)
-    
+
     print('Shape of class labels after conversion to 10-dimensional class matrix:')
     print(y_train.shape)
 
-
-    # Define the model 	
+    # Define the model
     model = Sequential()
     img_rows = img_cols = 28
     if K.image_data_format() == 'channels_first':
@@ -80,10 +78,10 @@ if __name__ == '__main__':
     model.add(Conv2D(32, 3, activation='relu', input_shape=input_shape))
     print("Shape of model's output:")
     print(model.output_shape)
-	
+
     # Add some more conv layers
     model.add(Conv2D(32, 3, activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     # Add a FCNN at the end
@@ -101,7 +99,7 @@ if __name__ == '__main__':
 
     # Train!
     print('Starting training')
-    model.fit(X_train, Y_train, 
+    model.fit(X_train, Y_train,
               batch_size=32, epochs=10, verbose=1)
     # Evaluate
     score = model.evaluate(X_test, Y_test, verbose=0)
@@ -109,6 +107,5 @@ if __name__ == '__main__':
 
     # Save the model
     MODEL_FILENAME = 'mnist.h5'
-    print ('Saving trained model as ' + MODEL_FILENAME)
+    print('Saving trained model as ' + MODEL_FILENAME)
     model.save(MODEL_FILENAME)
-    
